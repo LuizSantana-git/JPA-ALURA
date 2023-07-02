925edd05d6a38da7cb900d11ac21a9678339f93b
LuizGSSantana <84251111+LuizGSSantana@users.noreply.github.com>
2023-07-02 13:40:25 -0300

    added testes

---
+++ AluraJPACurso/src/main/java/testes/CadastroDeProduto.java
@@ -0,0 +1,25 @@
+package testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.Produto;
+
+public class CadastroDeProduto {
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+		celular.setNome("xiaomi redmi");
+		celular.setDescricao("muito legal");
+		celular.setPreco(new BigDecimal ("800"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		EntityManager em = factory.createEntityManager();
+		
+		em.persist(celular);
+		
+	}
+
+}
